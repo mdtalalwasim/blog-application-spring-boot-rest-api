c34b4c15baf1293a89ee10206398c8e67dc6049b
Md. Talal Wasim <md.talalwasim@gmail.com>
2023-09-06 22:28:45 +0600

    Post Entity is created.

---
+++ blog-application-springboot-rest-apis/src/main/java/com/mdtalalwasim/blog/app/entity/Post.java
@@ -0,0 +1,40 @@
+package com.mdtalalwasim.blog.app.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "posts")
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "post_title", length = 100, nullable = false)
+	private String postTitle;
+	
+	@Column(name = "post_content")
+	private String postContent;
+	
+	@Column(name = "post_image")
+	private String postImage;
+	
+	@Column(name = "post_date")
+	private Date postCreatedDate;
+	
+}
