6e82197e01c130fc1a9c17615efc47c8b09a39c9
Md. Talal Wasim <md.talalwasim@gmail.com>
2023-10-11 01:46:48 +0600

    Create Separate Class for all Constants.

---
+++ blog-application-springboot-rest-apis/src/main/java/com/mdtalalwasim/blog/app/config/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.mdtalalwasim.blog.app.config;
+
+public class ApplicationConstants {
+	
+	public static final String PAGE_NUMBER ="0";
+	public static final String PAGE_SIZE ="5";
+	public static final String SORT_BY ="id";
+
+}
--- blog-application-springboot-rest-apis/src/main/java/com/mdtalalwasim/blog/app/controller/PostRestController.java
+++ blog-application-springboot-rest-apis/src/main/java/com/mdtalalwasim/blog/app/controller/PostRestController.java
@@ -3,6 +3,7 @@ package com.mdtalalwasim.blog.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mdtalalwasim.blog.app.config.ApplicationConstants;
 import com.mdtalalwasim.blog.app.payloads.ApiResponse;
 import com.mdtalalwasim.blog.app.payloads.PostDto;
 import com.mdtalalwasim.blog.app.payloads.PostResponse;
@@ -59,9 +61,9 @@ public class PostRestController {
 	// get all post
 	@GetMapping("/all-post")
 	public ResponseEntity<PostResponse> getAllPost(
-			@RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber, 
-			@RequestParam(value = "pageSize", defaultValue = "5", required = false) Integer pageSize, 
-			@RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy) {
+			@RequestParam(value = "pageNumber", defaultValue = ApplicationConstants.PAGE_NUMBER, required = false) Integer pageNumber, 
+			@RequestParam(value = "pageSize", defaultValue = ApplicationConstants.PAGE_SIZE, required = false) Integer pageSize, 
+			@RequestParam(value = "sortBy", defaultValue = ApplicationConstants.SORT_BY, required = false) String sortBy) {
 
 		PostResponse postResponse = this.postService.getAllPost(pageNumber, pageSize, sortBy);
 
