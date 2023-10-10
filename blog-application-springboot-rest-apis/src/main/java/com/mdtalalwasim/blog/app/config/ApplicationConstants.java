002f488126bafa514a163a28b2b2582e228f0d72
Md. Talal Wasim <md.talalwasim@gmail.com>
2023-10-11 01:49:32 +0600

    Merge branch 'main' of https://github.com/mdtalalwasim/blog-application-spring-boot-rest-api.git into main. Implement Class for All hard coded Constants.(2.7)

--- blog-application-springboot-rest-apis/src/main/java/com/mdtalalwasim/blog/app/config/ApplicationConstants.java
+++ blog-application-springboot-rest-apis/src/main/java/com/mdtalalwasim/blog/app/config/ApplicationConstants.java
@@ -5,5 +5,8 @@ public class ApplicationConstants {
 	public static final String PAGE_NUMBER ="0";
 	public static final String PAGE_SIZE ="5";
 	public static final String SORT_BY ="id";
+	
+	
+	
 
 }
