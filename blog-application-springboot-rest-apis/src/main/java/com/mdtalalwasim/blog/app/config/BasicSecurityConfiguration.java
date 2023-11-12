fd7f64bea41b15a87f9f1353cb240e7489c9f343
Md. Talal Wasim <md.talalwasim@gmail.com>
2023-11-13 00:37:11 +0600

    some changes... pom.xml

--- blog-application-springboot-rest-apis/src/main/java/com/mdtalalwasim/blog/app/config/BasicSecurityConfiguration.java
+++ blog-application-springboot-rest-apis/src/main/java/com/mdtalalwasim/blog/app/config/BasicSecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.mdtalalwasim.blog.app.security.CustomUserDetailsService;
 import com.mdtalalwasim.blog.app.security.JwtAuthenticationEntryPoint;
@@ -23,8 +25,10 @@ import com.mdtalalwasim.blog.app.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-//@EnableWebMvc
-@EnableGlobalMethodSecurity(prePostEnabled = true) //We can apply Security on each method by Annotating this.
+@EnableWebMvc
+//@EnableGlobalMethodSecurity(prePostEnabled = true) //We can apply Security on each method by Annotating this. 
+//for new spring boot version we can user instead @EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 public class BasicSecurityConfiguration{
 	//old technique is extends WebSecurityConfigurerAdapter
 	//new technique is:-> Spring Security without the WebSecurityConfigurerAdapter
@@ -53,6 +57,8 @@ public class BasicSecurityConfiguration{
 		
 		.authorizeHttpRequests()
         .requestMatchers("/api/auth/**").permitAll()
+        //.requestMatchers("/v3/api-docs", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+        
         
         //now, any one can access GET APIs. No need to login. Now, GET APIs can access publicly
         .requestMatchers(HttpMethod.GET).permitAll()  
